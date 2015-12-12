cf3c69a4c7f4390a35aef4c7fac8a04b6f3d36f3
Joel <jdeanjj1000@gmail.com>
2015-12-11 19:19:57 -0500

    added screenshot detection via content observer

--- library/src/main/java/com/screen/watch/ScreenWatch.java
+++ library/src/main/java/com/screen/watch/ScreenWatch.java
@@ -1,8 +1,15 @@
 package com.screen.watch;
 
+import android.app.Activity;
+import android.database.ContentObserver;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.FileObserver;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import java.io.File;
@@ -79,4 +86,59 @@ public class ScreenWatch extends FileObserver {
     }
 
 
+            HandlerThread handlerThread = new HandlerThread("content_observer");
+    //handlerThread.start();
+    final Handler handler = new Handler(handlerThread.getLooper()) {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
+
+    public void useContentObserver(final Activity activity)
+    {
+    activity.getContentResolver().registerContentObserver(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            true,
+                    new ContentObserver(handler) {
+        @Override
+        public boolean deliverSelfNotifications() {
+            Log.d(TAG, "deliverSelfNotifications");
+            return super.deliverSelfNotifications();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            Log.d(TAG, "onChange " + uri.toString());
+            if (uri.toString().matches(MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString() + "/[0-9]+")) {
+
+                Cursor cursor = null;
+                try {
+                    cursor = activity.getContentResolver().query(uri, new String[] {
+                            MediaStore.Images.Media.DISPLAY_NAME,
+                            MediaStore.Images.Media.DATA
+                    }, null, null, null);
+                    if (cursor != null && cursor.moveToFirst()) {
+                        final String fileName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
+                        final String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                        // TODO: apply filter on the file name to ensure it's screen shot event
+                        Log.d(TAG, "screen shot added " + fileName + " " + path);
+                    }
+                } finally {
+                    if (cursor != null)  {
+                        cursor.close();
+                    }
+                }
+            }
+            super.onChange(selfChange, uri);
+        }
+    }
+    );
+    }
 }
