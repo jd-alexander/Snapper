9c2fd77ac643955b30f37640301a15711bb2a7ef
Joel <jdeanjj1000@gmail.com>
2015-12-13 19:19:05 -0500

    added modes to snapper so that either file observer or content observer can be used

---
+++ library/src/main/java/com/snapper/Mode.java
@@ -0,0 +1,9 @@
+package com.snapper;
+
+/**
+ * Created by Joel on 13/12/2015.
+ */
+public enum Mode {
+    FILEOBSERVER,
+    CONTENTOBSERVER
+}
--- library/src/main/java/com/snapper/Snapper.java
+++ library/src/main/java/com/snapper/Snapper.java
@@ -21,64 +21,128 @@ public class Snapper extends FileObserver {
     private static final String TAG = "Snapper";
     private static final String PATH = Environment.getExternalStorageDirectory().toString() + "/Pictures/Screenshots/";
 
-    private boolean deleteScreenshot=false;
+    private boolean deleteScreenshot = false;
     private OnScreenshotTakenListener mListener;
     private String mLastTakenPath;
+    private Activity activity;
+    private ContentObserver contentObserver;
+
+    private Mode watchMode;
 
     public Snapper(OnScreenshotTakenListener listener) {
         super(PATH, FileObserver.CLOSE_WRITE);
         mListener = listener;
+        watchMode = Mode.FILEOBSERVER;
     }
 
     public Snapper() {
         super(PATH, FileObserver.CLOSE_WRITE);
+        watchMode = Mode.FILEOBSERVER;
+
     }
 
-    public void setWatcher(OnScreenshotTakenListener listener)
-    {
-        mListener=listener;
+    public void useContentObserver(final Activity activity) {
+        watchMode = Mode.CONTENTOBSERVER;
+        this.activity=activity;
+        contentObserver=new ContentObserver(handler) {
+            @Override
+            public boolean deliverSelfNotifications() {
+                Log.d(TAG, "deliverSelfNotifications");
+                return super.deliverSelfNotifications();
+            }
+
+            @Override
+            public void onChange(boolean selfChange) {
+                super.onChange(selfChange);
+            }
+
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                Log.d(TAG, "onChange " + uri.toString());
+                if (uri.toString().matches(MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString() + "/[0-9]+")) {
+
+                    Cursor cursor = null;
+                    try {
+                        cursor = activity.getContentResolver().query(uri, new String[]{
+                                MediaStore.Images.Media.DISPLAY_NAME,
+                                MediaStore.Images.Media.DATA
+                        }, null, null, null);
+                        if (cursor != null && cursor.moveToFirst()) {
+                            final String fileName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
+                            final String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                            // TODO: apply filter on the file name to ensure it's screen shot event
+                            Log.d(TAG, "screen shot added " + fileName + " " + path);
+                        }
+                    } finally {
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                    }
+                }
+                super.onChange(selfChange, uri);
+            }
+        };
+
+    }
+
+    public Mode getWatchMode() {
+        return watchMode;
+    }
+
+
+    public void setWatcher(OnScreenshotTakenListener listener) {
+        mListener = listener;
     }
 
     @Override
     public void onEvent(int event, String path) {
         Log.i(TAG, "Event:" + event + "\t" + path);
 
-        if (path==null || event!=FileObserver.CLOSE_WRITE)
+        if (path == null || event != FileObserver.CLOSE_WRITE)
             Log.i(TAG, "Not important");
-        else if (mLastTakenPath!=null && path.equalsIgnoreCase(mLastTakenPath))
+        else if (mLastTakenPath != null && path.equalsIgnoreCase(mLastTakenPath))
             Log.i(TAG, "This event has been observed before.");
         else {
             mLastTakenPath = path;
-            File file = new File(PATH+path);
+            File file = new File(PATH + path);
 
-            if(deleteScreenshot)
-            {
-                if(file!=null)
+            if (deleteScreenshot) {
+                if (file != null)
                     file.delete();
 
                 /*
                 * A null uri is returned to listener once screenshot
                 * has been deleted.
                 * */
-                if(mListener!=null)
+                if (mListener != null)
                     mListener.onScreenshotTaken(null);
 
-            }
-            else
-            {
+            } else {
 
-                if(mListener!=null)
+                if (mListener != null)
                     mListener.onScreenshotTaken(Uri.fromFile(file));
             }
         }
     }
 
     public void start() {
-        super.startWatching();
+        if (watchMode.equals(Mode.FILEOBSERVER))
+            super.startWatching();
+        else if(watchMode.equals(Mode.CONTENTOBSERVER))
+            startContentObserver();
     }
 
     public void stop() {
-        super.stopWatching();
+        if (watchMode.equals(Mode.FILEOBSERVER))
+            super.stopWatching();
+        else if(watchMode.equals(Mode.CONTENTOBSERVER))
+            unregisterContentObserver();
+    }
+
+    public void unregisterContentObserver()
+    {
+        if(watchMode.equals(Mode.CONTENTOBSERVER))
+        activity.getContentResolver().unregisterContentObserver(contentObserver);
     }
 
     public void deleteScreenshot(boolean deleteScreenshot) {
@@ -86,7 +150,7 @@ public class Snapper extends FileObserver {
     }
 
 
-            HandlerThread handlerThread = new HandlerThread("content_observer");
+    HandlerThread handlerThread = new HandlerThread("content_observer");
     //handlerThread.start();
     final Handler handler = new Handler(handlerThread.getLooper()) {
 
@@ -96,49 +160,10 @@ public class Snapper extends FileObserver {
         }
     };
 
-    public void useContentObserver(final Activity activity)
-    {
-    activity.getContentResolver().registerContentObserver(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            true,
-                    new ContentObserver(handler) {
-        @Override
-        public boolean deliverSelfNotifications() {
-            Log.d(TAG, "deliverSelfNotifications");
-            return super.deliverSelfNotifications();
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            super.onChange(selfChange);
-        }
-
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            Log.d(TAG, "onChange " + uri.toString());
-            if (uri.toString().matches(MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString() + "/[0-9]+")) {
-
-                Cursor cursor = null;
-                try {
-                    cursor = activity.getContentResolver().query(uri, new String[] {
-                            MediaStore.Images.Media.DISPLAY_NAME,
-                            MediaStore.Images.Media.DATA
-                    }, null, null, null);
-                    if (cursor != null && cursor.moveToFirst()) {
-                        final String fileName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
-                        final String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-                        // TODO: apply filter on the file name to ensure it's screen shot event
-                        Log.d(TAG, "screen shot added " + fileName + " " + path);
-                    }
-                } finally {
-                    if (cursor != null)  {
-                        cursor.close();
-                    }
-                }
-            }
-            super.onChange(selfChange, uri);
-        }
-    }
-    );
+    public void startContentObserver() {
+        watchMode = Mode.CONTENTOBSERVER;
+        activity.getContentResolver().registerContentObserver(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                true,contentObserver);
     }
 }
